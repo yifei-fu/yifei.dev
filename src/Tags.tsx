9bde4848e3e0249a8e4ece47e03a2804488f90e3
yifei-fu <yifeifu2@gmail.com>
2019-08-18 01:23:14 -0700

    Component for rendering Tags.

---
+++ src/Tags.tsx
@@ -0,0 +1,27 @@
+import {Chip} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  chip: {
+    marginTop: theme.spacing(0.5),
+    marginRight: theme.spacing(0.5),
+  },
+}));
+
+export interface Props {
+  tags: string[];
+}
+
+const Tags: React.FC<Props> = (props: Props) => {
+  const classes = useStyles();
+  const {tags} = props;
+
+  return (
+    <>
+      {tags.map((tag, idx) => <Chip className={classes.chip} key={idx} variant='outlined' color='primary' label={tag}/>)}
+    </>
+  );
+};
+
+export default Tags;

b08da4c1b054639bfd94713db463694bc4e0c309
yifei-fu <yifeifu2@gmail.com>
2019-08-18 19:56:15 -0700

    Build v1.0.0 (initial release) to /docs.

---
+++ docs/precache-manifest.ccbbb218e18633f43b8bb7a31e280d7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddc2df2df07ba214163878d339b49fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53bee2cd02dab1144868",
+    "url": "/static/css/main.801541fa.chunk.css"
+  },
+  {
+    "revision": "4249f5e8463527780cca",
+    "url": "/static/js/2.8eb04485.chunk.js"
+  },
+  {
+    "revision": "53bee2cd02dab1144868",
+    "url": "/static/js/main.622b9b8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
